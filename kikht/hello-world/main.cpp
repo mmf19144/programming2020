5e7adc0cb9f2afbd95e3032f94e87bf5459474fe
RiliKulHacker <v.travnikov@g.nsu.ru>
2020-09-08 18:17:31 +0700

    init 2

--- kikht/hello-world/main.cpp
+++ kikht/hello-world/main.cpp
@@ -3,13 +3,10 @@
 int main(int argc, char** argv) {
     std::cout << "Hello, ";
     if (argc > 1) {
-        for (size_t i = 1; i < argc-1; i++) {
-            std::cout << argv[1] << " ";
-        }
-        std::cout << argv[argc-1];
+        std::cout << argv[1];
     } else {
-        std::cout << "world";//esketit
+        std::cout << "world";
     }
     std::cout << "!" << std::endl;
-    return 0;//test1
+    return 0;
 }
