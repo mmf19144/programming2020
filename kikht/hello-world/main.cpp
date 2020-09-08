4b5ca50276074888cef93790373fd45d3a5aedf0
Vladimir Kikhtenko <kikht@yandex-team.ru>
2020-09-08 12:43:40 +0700

    Add name

--- kikht/hello-world/main.cpp
+++ kikht/hello-world/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+int main(int argc, char** argv) {
+    std::cout << "Hello, ";
+    if (argc > 1) {
+        std::cout << argv[1];
+    } else {
+        std::cout << "world";
+    }
+    std::cout << "!" << std::endl;
     return 0;
 }
