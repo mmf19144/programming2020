2f0f87e2b899e003810328dcb42b78cde58339fa
RiliKulHacker <v.travnikov@g.nsu.ru>
2020-10-05 21:23:08 +0700

    Matrix code final

--- Travnikov/matrix.cpp
+++ Travnikov/matrix.cpp
@@ -1,14 +1,15 @@
+#define CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 #include <stdio.h>
-
 using namespace std;
 
 
 class Matrix {
     int *data;
     int dim;
-    int len;
+
 public:
 
     friend void readMatrix(std::ifstream &is, Matrix &m);
@@ -16,15 +17,18 @@ public:
     friend void writeMatrix(std::ofstream &os, Matrix &m);
 
 
-    Matrix() {}
-
+    Matrix() {
+        this->dim = 0;
+        this->data = nullptr;
+    }
     ~Matrix() {
         delete[] data;
+
     }
 
     Matrix(int num, bool singular = true) {
         this->dim = num;
-        this->len = num * num;
+
         this->data = new int[num * num];
 
         for (int i = 0; i < num; i++) {
@@ -36,9 +40,9 @@ public:
         //for (int i = 0; i < num; i++) { this->data[i][i] = 1; }
     }
 
-    Matrix(int num, int *arr) {
+    Matrix(int num, int const *arr) {
         this->dim = num;
-        this->data = new int[num];
+        this->data = new int[num*num];
 
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
@@ -52,7 +56,7 @@ public:
 
     Matrix(int num, int de) {
         this->dim = num;
-        this->data = new int[num];
+        this->data = new int[num*num];
 
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
@@ -64,9 +68,7 @@ public:
         }
     }
 
-    int getElem(int a, int b) {
-        return this->data[a * (this->dim) + b];
-    }
+
 
     Matrix operator+(const Matrix &that) const {
         if (this->dim != that.dim) {
@@ -110,20 +112,9 @@ public:
         return result;
     }
 
-    Matrix operator^(const Matrix &that) const {
-        for (int i = 0; i < this->dim; i++) {
-            for (int j = 0; j < this->dim; j++) {
-                if (this->data[i * this->dim + j] != that.data[i * this->dim + j]) {
-                    //printf("F");
-                    return false;
-                }
-            }
-        }
-        //printf("T");
-        return true;
-    }
 
-    Matrix operator!() {
+
+    Matrix operator!() const {
         Matrix result(this->dim);
         for (int i = 0; i < this->dim; i++) {
             for (int j = 0; j < this->dim; j++) {
@@ -164,10 +155,8 @@ public:
         return result;
     }
 
-    /*Matrix operator[](int row){
-        Matrix result(this->dim,false);
-        or(int n=0; n<this->dim; n++) result[n] = this->data[row*this->dim+n]
-    }*/
+
+
 };
 
 void readMatrix(std::ifstream &is, Matrix &m) {
@@ -181,7 +170,7 @@ void readMatrix(std::ifstream &is, Matrix &m) {
 void writeMatrix(std::ofstream &os, Matrix &m) {
     for (int i = 0; i < m.dim; i++) {
         for (int j = 0; j < m.dim; j++) {
-            os << m.data[i * m.dim + j] << " ";
+            os << m.data[i * m.dim + j]<< " " ;
         }
         os << endl;
     }
@@ -189,19 +178,7 @@ void writeMatrix(std::ofstream &os, Matrix &m) {
 
 
 int main() {
-    int a = 0;
-    int tstarr1[4] = {1, 2, 3, 4};
-    int tstarr2[4] = {5, 6, 7, 8};
-    /*Matrix mtx1(4, tstarr1);
-    Matrix mtx2(4, tstarr2);
-    Matrix mtx3 =  mtx2 (1,3);
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d", mtx3.getElem(i, j));
-        }
-        printf("\n");
 
-    }*/
 
     ifstream fin("input.txt");
 
@@ -219,27 +196,19 @@ int main() {
     readMatrix(fin, C);
     readMatrix(fin, D);
 
-    ofstream fout("result.txt");
+    ofstream fout("output.txt");
 
     Matrix K(msize, k);
-    Matrix R(msize, false);
 
-    R = (A + B * !C + K) * !D;
+
+    Matrix R = (A + B * (!C) + K) * (!D);
 
     writeMatrix(fout, R);
-    //fout <<R[1][1] << "\t"<< msize;
+
 
     fin.close();
     fout.close();
 
-
-
-
-
-
-    //scanf("%d",&a);
-    //std::cout << "Hello, World!" << std::endl;
-    //printf("%d",a);
     return 0;
 
 
