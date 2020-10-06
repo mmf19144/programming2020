729ee8faaa178db5c2666672ecc89175d9539af3
Михаил Тютюльников <mishatyty@gmail.com>
2020-10-06 15:59:17 +0700

    matrix fixes

--- m_tyuytulnikov/matrix.cpp
+++ m_tyuytulnikov/matrix.cpp
@@ -1,230 +1,237 @@
-#include <cassert>
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
-using namespace std;
 
 class Row {
-    size_t row_id;
-    size_t matrix_size;
-    int * matrix_data;
+    const size_t row_id;
+    const size_t matrix_size;
+    int *const matrix_data;
+
 public:
-    Row(size_t idx, size_t size, int * data)
-            : row_id(idx)
-            , matrix_size(size)
-            , matrix_data(data){
+    Row(const size_t idx, const size_t size, int *const data)
+            : row_id(idx), matrix_size(size), matrix_data(data) {
     }
 
-    int & operator[](size_t col_id){
-        col_id--;
-        if (col_id > matrix_size || col_id <0){
+    int &operator[](size_t col_id) {
+        if (col_id > matrix_size || col_id < 1) {
             throw std::invalid_argument("Invalid column index");
         }
-        return matrix_data[row_id*matrix_size+col_id];
+        col_id--;
+
+        return matrix_data[row_id * matrix_size + col_id];
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, const Row &row) {
+        for (size_t i = 0; i < row.matrix_size; i++) {
+            stream << row.matrix_data[row.row_id * row.matrix_size + i];
+            if (i + 1 == row.matrix_size)
+                break;
+            stream << " ";
+        }
+        return stream;
     }
 };
 
-class Column{
-    size_t col_id;
-    size_t matrix_size;
-    int * matrix_data;
+class Column {
+    const size_t col_id;
+    const size_t matrix_size;
+    int *const matrix_data;
 public:
-    Column(size_t idx, size_t size, int * data)
-            : col_id(idx)
-            , matrix_size(size)
-            , matrix_data(data){
+    Column(const size_t idx, const size_t size, int *const data)
+            : col_id(idx), matrix_size(size), matrix_data(data) {
     }
 
-    int & operator[](size_t row_id){
-        row_id--;
-        if (row_id > matrix_size || row_id <0){
+    int &operator[](size_t row_id) {
+        if (row_id > matrix_size || row_id < 1) {
             throw std::invalid_argument("Invalid row index");
         }
-        return matrix_data[row_id*matrix_size+col_id];
+        row_id--;
+
+        return matrix_data[row_id * matrix_size + col_id];
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, const Column &col) {
+        for (size_t i = 0; i < col.matrix_size; i++) {
+            stream << col.matrix_data[i * col.matrix_size + col.col_id];
+            if (i + 1 == col.matrix_size)
+                break;
+            stream << " ";
+        }
+        return stream;
     }
 };
 
-class Matrix{
+class Matrix {
     const size_t matrix_size;
-    int * const data;
+    int *const data;
+
 public:
     Matrix()
-            : matrix_size(0)
-            , data(nullptr)
-    {
+            : matrix_size(0), data(nullptr) {
     }
 
-    Matrix(size_t size)
-            : matrix_size(size)
-            , data(new int[matrix_size*matrix_size]())
-    {
-        for (size_t i = 0; i<matrix_size*matrix_size; i+=(matrix_size+1)){
-            data[i]=1;
+    explicit Matrix(const size_t size)
+            : matrix_size(size), data(new int[matrix_size * matrix_size]()) {
+        memset(data, 0, matrix_size * matrix_size * sizeof(int));
+        for (size_t i = 0; i < matrix_size * matrix_size; i += (matrix_size + 1)) {
+            data[i] = 1;
         }
     }
 
-    Matrix (size_t size, const int * arr)
-            : matrix_size(size)
-            , data(new int[matrix_size*matrix_size]())
-    {
-        for (size_t i = 0, j = 0; i<matrix_size*matrix_size; i+=(matrix_size+1), j++ ){
-            data[i]=arr[j];
-        }
-    }
-
-    Matrix (size_t size, ifstream& stream)
-            :matrix_size(size)
-            , data(new int[matrix_size*matrix_size]())
-    {
-        for (int i = 0; i< matrix_size*matrix_size; i++){
-            stream >> data[i];
+    Matrix(const size_t size, const int *arr)
+            : matrix_size(size), data(new int[matrix_size * matrix_size]()) {
+        memset(data, 0, matrix_size * matrix_size * sizeof(int));
+        for (size_t i = 0, j = 0; i < matrix_size * matrix_size; i += (matrix_size + 1), j++) {
+            data[i] = arr[j];
         }
     }
 
 
-    ~Matrix(){
+    ~Matrix() {
         delete[] data;
     }
 
-    Matrix operator+(const Matrix& other){
-        if (matrix_size != other.matrix_size){
+    Matrix operator+(const Matrix &other) {
+        if (matrix_size != other.matrix_size) {
             throw std::invalid_argument("Invalid size of matrix ");
         }
         Matrix result_matrix(matrix_size);
-        for (size_t i =0; i<matrix_size*matrix_size; i++){
-            result_matrix.data[i]=data[i]+other.data[i];
+        for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+            result_matrix.data[i] = data[i] + other.data[i];
         }
         return result_matrix;
     }
 
-    Matrix operator-(const Matrix& other){
-        if (matrix_size != other.matrix_size){
+    Matrix operator-(const Matrix &other) {
+        if (matrix_size != other.matrix_size) {
             throw std::invalid_argument("Invalid size of matrix ");
         }
-        Matrix result_matrix (matrix_size);
-        for (size_t i =0; i<matrix_size*matrix_size; i++){
-            result_matrix.data[i]=data[i]-other.data[i];
+        Matrix result_matrix(matrix_size);
+        for (size_t i = 0; i < matrix_size * matrix_size; i++) {
+            result_matrix.data[i] = data[i] - other.data[i];
         }
         return result_matrix;
     }
 
-    Matrix operator*(const Matrix& other){
-        if (matrix_size != other.matrix_size){
+    Matrix operator*(const Matrix &other) {
+        if (matrix_size != other.matrix_size) {
             throw std::invalid_argument("Invalid size of matrix ");
         }
         Matrix result_matrix(matrix_size);
-        for (size_t row_id = 0; row_id< matrix_size; row_id++){
-            for (size_t col_id = 0; col_id< matrix_size; col_id++){
-                result_matrix.data[row_id*matrix_size+col_id] = 0;
-                for (size_t k = 0; k< matrix_size; k++){
-                    result_matrix.data[row_id*matrix_size+col_id] += data[row_id*matrix_size+k]*other.data[k*matrix_size+col_id];
+
+        for (size_t row_id = 0; row_id < matrix_size; row_id++) {
+            for (size_t col_id = 0; col_id < matrix_size; col_id++) {
+                result_matrix[row_id + 1][col_id + 1] = 0;
+                for (size_t k = 0; k < matrix_size; k++) {
+                    result_matrix[row_id + 1][col_id + 1] +=
+                            (*this)[row_id + 1][k + 1] * other[k + 1][col_id + 1];
                 }
             }
         }
         return result_matrix;
     }
 
-    bool operator==(const Matrix& other){
-        if (matrix_size != other.matrix_size){
+    bool operator==(const Matrix &other) {
+        if (matrix_size != other.matrix_size) {
             throw std::invalid_argument("Invalid size of matrix ");
         }
-        for (size_t i =0; i<matrix_size*matrix_size; i++){
-            if (data[i]!=other.data[i]){
-                return false;
-            }
-        }
-        return true;
+        return memcmp(data, other.data, matrix_size * matrix_size * sizeof(int)) == 0;
     }
 
-    bool operator!=(const Matrix& other){
+    bool operator!=(const Matrix &other) {
         return !(*this == other);
     }
 
-    Matrix operator() (size_t row, size_t col){
-        row--;
-        col--;
-        if (col > matrix_size || row > matrix_size || col <0 || row < 0){
+    Matrix operator()(size_t row, size_t col) {
+        if (col > matrix_size || row > matrix_size || col < 1 || row < 1) {
             throw std::invalid_argument("Invalid column and row indexes");
         }
-        if (matrix_size <1){
+        row--;
+        col--;
+        if (matrix_size < 1) {
             throw std::invalid_argument("Invalid matrix size");
         }
-        Matrix result_matrix(matrix_size-1);
-        for (int i = 0; i < matrix_size; i++){
+        Matrix result_matrix(matrix_size - 1);
+        for (size_t i = 0; i < matrix_size; i++) {
             if (i == row) continue;
-            for (int j = 0; j< matrix_size; j++){
+            for (size_t j = 0; j < matrix_size; j++) {
                 if (j == col) continue;
-                size_t row_shift = 0;
-                size_t col_shift = 0;
-                if (i > row) row_shift=-1;
-                if (j > col) col_shift=-1;
-                result_matrix.data[(i+row_shift)*(matrix_size-1)+(j+col_shift)] = data[i*matrix_size+j];
+                int row_shift = 0;
+                int col_shift = 0;
+                if (i > row) row_shift = -1;
+                if (j > col) col_shift = -1;
+                result_matrix[i + row_shift + 1][j + col_shift + 1] = (*this)[i+1][j+1];
             }
         }
         return result_matrix;
     }
 
-    Column operator()(size_t col_id){
-        col_id--;
-        if (col_id > matrix_size || col_id <0 ){
+    Column operator()(size_t col_id) const {
+        if (col_id > matrix_size || col_id < 1) {
             throw std::invalid_argument("Invalid column index");
         }
+        col_id--;
         return {col_id, matrix_size, data};
     }
 
-    Row operator[](size_t row_id){
-        row_id--;
-        if (row_id > matrix_size || row_id <0 ){
+    Row operator[](size_t row_id) const {
+        if (row_id > matrix_size || row_id < 1) {
             throw std::invalid_argument("Invalid row index");
         }
+        row_id--;
         return {row_id, matrix_size, data};
     }
 
 
-    Matrix transposed (){
+    Matrix transposed() {
         Matrix result_matrix(matrix_size);
-        for (size_t i = 0; i<matrix_size; i++){
-            for (size_t j = 0; j< matrix_size; j++){
-                result_matrix.data[i*matrix_size+j] = data[j*matrix_size+i];
+        for (size_t i = 0; i < matrix_size; i++) {
+            for (size_t j = 0; j < matrix_size; j++) {
+                result_matrix[i+1][j+1] = (*this)[j+1][i+1];
             }
         }
         return result_matrix;
     }
 
-    void Print(ofstream& stream) const {
-        for (size_t i = 0; i<matrix_size; i++){
-            for (size_t j = 0; j<matrix_size; j++){
-                stream << data[i*matrix_size+j] << " ";
-            }
+
+    friend std::ostream &operator<<(std::ostream &stream, const Matrix &matrix) {
+        for (size_t i = 0; i < matrix.matrix_size; i++) {
+            stream << matrix[i + 1];
             stream << "\n";
         }
+        return stream;
     }
 
+    friend std::istream &operator>>(std::istream &stream, const Matrix &matrix) {
+        for (size_t i = 0; i < matrix.matrix_size * matrix.matrix_size; i++) {
+            stream >> matrix.data[i];
+        }
+        return stream;
+    }
 
 };
 
-int main(){
-    ifstream input("input.txt");
-    ofstream output("output.txt");
-    int n, k;
+int main() {
+    std::ifstream input("input.txt");
+    std::ofstream output("output.txt");
+    size_t n;
+    int k;
     input >> n >> k;
 
-    int * k_arr = new int[n];
-    for (int i = 0; i<n;i++){
-        k_arr[i]=k;
+    int *k_arr = new int[n];
+    for (size_t i = 0; i < n; i++) {
+        k_arr[i] = k;
     }
 
-    Matrix A(n, input);
-    Matrix B(n, input);
-    Matrix C(n, input);
-    Matrix D(n, input);
+    Matrix A(n);
+    Matrix B(n);
+    Matrix C(n);
+    Matrix D(n);
     Matrix K(n, k_arr);
+    input >> A >> B >> C >> D;
 
-    ((A+(B*C.transposed()) + K) * D.transposed()).Print(output);
-//    A[1][2]=5;
-//    B(1)[2]=9;
-//    A.Print(output);
-//    B.Print(output);
+    output << ((A + (B * C.transposed()) + K) * D.transposed());
     input.close();
     output.close();
     return 0;
