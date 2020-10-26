170f0141f4d8248e369c0809e36c185714001deb
iprok <i.prokofev@g.nsu.ru>
2020-10-26 22:12:39 +0700

    tested

--- Ilya_prokofev/MatrixClass.cpp
+++ Ilya_prokofev/MatrixClass.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <algorithm>
 #include <fstream>
 using namespace std;
 
 class column
 {
-	int * matrix ;
+	int* matrix;
 	const size_t dim;
 	const size_t column_idx;
 
@@ -14,11 +13,11 @@ public:
 
 	int& operator[](const size_t row)//column element reference
 	{
-		if (row > dim) 
+		if (row > dim)
 		{
 			throw invalid_argument("Row index is greater than dimension");
 		}
-		return matrix[dim * (row-1) + column_idx - 1];
+		return matrix[dim * (row - 1) + column_idx - 1];
 	}
 };
 
@@ -37,9 +36,9 @@ public:
 		{
 			throw invalid_argument("Column index is greater than dimension");
 		}
-		return matrix[dim * (column-1) + row_idx - 1];
+		return matrix[dim * (column - 1) + row_idx - 1];
 	}
-	friend ostream& operator<< (ostream& os, const row &m) {
+	friend ostream& operator<< (ostream& os, const row& m) {
 		for (size_t i = 0; i < m.dim; i++) {
 			os << m.matrix[m.row_idx * m.dim + i];
 			os << " ";
@@ -52,16 +51,16 @@ class Matrix
 {
 	int* data; //matrix
 	size_t dim; //dimension
-	static int* allocate_matrix (int n) // memory allocation
+	static int* allocate_matrix(int n) // memory allocation
 	{
-		int* L = new int [n*n];
+		int* L = new int[n * n];
 		for (size_t i = 0; i < n * n; i++) {
 			L[i] = 0;
 		}
 		return L;
 	}
 public:
-	Matrix(): dim(0), data(nullptr) //default constructor
+	Matrix() : dim(0), data(nullptr) //default constructor
 	{}
 	Matrix(size_t n) : dim(n) //identity matrix constructor
 	{
@@ -71,7 +70,7 @@ public:
 			data[i * dim + i] = 1;
 		}
 	}
-	Matrix(size_t n, int m) : dim(n) 
+	Matrix(size_t n, int m) : dim(n)
 	{
 		data = allocate_matrix(n);
 		for (size_t i = 0; i < n; i++)
@@ -87,12 +86,12 @@ public:
 			data[i * dim + i] = D[i];
 		}
 	}
-	Matrix(const Matrix& that) : dim (that.dim) //copy constructor
+	Matrix(const Matrix& that) : dim(that.dim) //copy constructor
 	{
 		this->data = allocate_matrix(dim);
 		for (size_t i = 0; i < dim * dim; i++)
 		{
-			this -> data[i] = that.data[i];
+			this->data[i] = that.data[i];
 		}
 	}
 	void clear_matrix()
@@ -112,7 +111,7 @@ public:
 		this->data = allocate_matrix(dim);
 		for (size_t i = 0; i < dim; i++)
 		{
-			this -> data[i] = that.data[i];
+			this->data[i] = that.data[i];
 		}
 		return *this;
 	}
@@ -125,9 +124,9 @@ public:
 		int n1 = this->dim;
 		Matrix temp(n1);
 
-		for (size_t i = 0; i < n1*n1; i++)
+		for (size_t i = 0; i < n1 * n1; i++)
 		{
-			temp.data[i] = this -> data[i] + that.data[i];
+			temp.data[i] = this->data[i] + that.data[i];
 		}
 		return temp;
 	}
@@ -154,9 +153,9 @@ public:
 		}
 		size_t n1 = this->dim;
 		Matrix temp(n1);
-		for (size_t i = 0; i < n1; i++)
+		for (size_t i = 0; i < n1*n1; i++)
 		{
-				temp.data[i] = this -> data[i] - that.data[i];
+			temp.data[i] = this->data[i] - that.data[i];
 		}
 		return temp;
 	}
@@ -168,14 +167,14 @@ public:
 		}
 		size_t n1 = this->dim;
 		Matrix temp(n1);
-		for (size_t i = 0; i < n1; i++)
+		for (size_t i = 1; i <= n1; i++)
 		{
-			for (size_t j = 0; j < n1; j++)
+			for (size_t j = 1; j <= n1; j++)
 			{
 				temp[i][j] = 0;
-				for (size_t k = 0; k < n1; k++)
+				for (size_t k = 1; k <= n1; k++)
 				{
-					temp[i][j] += (*this)[i][k] * that[k][j];
+					temp[i][j] += (*this)[k][j] * that[i][k];
 				}
 			}
 		}
@@ -189,7 +188,7 @@ public:
 		}
 		for (size_t i = 0; i < dim; i++)
 		{
-			if (this -> data[i] != that.data[i])
+			if (this->data[i] != that.data[i])
 			{
 				return false;
 			}
@@ -203,9 +202,9 @@ public:
 	Matrix operator!() const //matrix transpose
 	{
 		Matrix res(*this);
-		for(size_t i = 0; i < dim; i++)
+		for (size_t i = 1; i <= dim; i++)
 		{
-			for (size_t j = i+1; j < dim; j++)
+			for (size_t j = i + 1; j <= dim; j++)
 			{
 				swap(res[i][j], res[j][i]);
 			}
@@ -222,33 +221,33 @@ public:
 		{
 			throw invalid_argument("column index is greater than dim");
 		}
-		Matrix res(this -> dim-1);
-		for (size_t i = 0; i < raw-1; i++)
+		Matrix res(this->dim - 1);
+		for (size_t i = 0; i < raw - 1; i++)
 		{
-			for (size_t j = 0; i < column-1; j++)
+			for (size_t j = 0; i < column - 1; j++)
 			{
 				res[i][j] = (*this)[i][j];
 			}
 		}
-		for (size_t i = raw; i < dim-1; i++)
+		for (size_t i = raw; i < dim - 1; i++)
 		{
 			for (size_t j = 0; i < column - 1; j++)
 			{
-				res[i][j] = (*this)[i+1][j];
+				res[i][j] = (*this)[i + 1][j];
 			}
 		}
 		for (size_t i = 0; i < raw - 1; i++)
 		{
 			for (size_t j = column; i < dim - 1; j++)
 			{
-				res[i][j] = (*this)[i][j+1];
+				res[i][j] = (*this)[i][j + 1];
 			}
 		}
 		for (size_t i = raw; i < dim - 1; i++)
 		{
 			for (size_t j = column; i < dim - 1; j++)
 			{
-				res[i][j] = (*this)[i + 1][j+1];
+				res[i][j] = (*this)[i + 1][j + 1];
 			}
 		}
 		return res;
@@ -259,7 +258,7 @@ public:
 		{
 			throw invalid_argument("raw is  greater than dim");
 		}
-		return { dim, data, index};
+		return { dim, data, index };
 	}
 
 	column operator()(const size_t index)//column access
@@ -269,14 +268,14 @@ public:
 			throw invalid_argument("column is  greater than dim");
 		}
 
-		return { dim, data, index};
+		return { dim, data, index };
 	}
 
 	friend ostream& operator<<(ostream& os, const Matrix& m) {
-			for (size_t i = 0; i < m.dim; i++) {
-				os << m[i];
-				os << "\n";
-			}
+		for (size_t i = 0; i < m.dim; i++) {
+			os << m[i];
+			os << "\n";
+		}
 		return os;
 	}
 
@@ -304,13 +303,6 @@ int main()
 	input >> A >> B >> C >> D;
 	input.close();
 	Matrix K(dim, k);
-	//A.print(); B.print(); C.print(); D.print(); K.print();
-	//((A + (B * (!C)) + K) * (!D)).print(output);
-	//A += B;
-	//A.print();
-	A[1][1] -= 8;
-	A(2)[2] += 5;
-	output << A;
-	output.close();
+	output << ((A + (B * (!C)) + K) * (!D));
 	return 0;
 }
