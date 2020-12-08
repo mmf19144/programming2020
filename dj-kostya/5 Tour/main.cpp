e5d94599e0f7cf1d165cd246fa225995ebce1ec1
k.nosorev <k.nosorev@g.nsu.ru>
2020-12-08 16:09:15 +0700

    Rewrite with safety queue

--- dj-kostya/5 Tour/main.cpp
+++ dj-kostya/5 Tour/main.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 #include <regex>
 #include <exception>
-#include <unordered_set>
+#include <vector>
 #include <queue>
 #include <mutex>
 #include <thread>
 #include <chrono>
+#include <condition_variable>
+#include <utility>
+#include <unordered_set>
 
 using namespace std;
 
@@ -18,102 +21,155 @@ using namespace std;
 
 //#define DEBUG
 
-const string ROOT_DIR = "";
+string ROOT_DIR = "test_data";
 
-mutex pushLock;
+template<class Uniq>
+class IJob {
+public:
+    virtual ~IJob() = default;
 
-vector<bool> threadStatus;
+    virtual Uniq &getUniq() = 0;
 
-string getBodyByHref(const string &href) {
-    string delimiter = "://";
-    auto finding = href.find(delimiter);
-    if (finding == -1) {
+    virtual void Execute() = 0;
+};
+
+template<class T, class Uniq>
+class Queue {
+public:
+    void Push(std::unique_ptr<T> job) {
+        std::unique_lock<std::mutex> guard(lock);
+        if (set.find(job->getUniq()) == set.end()) {
+            set.insert(job->getUniq());
+            queue.emplace(std::move(job));
+            addedJobs++;
+            cond.notify_one();
+        }
+    }
+
+    std::unique_ptr<T> TryPop() {
+        std::unique_lock<std::mutex> guard(lock);
+        if (stop || queue.empty()) {
+            return nullptr;
+        }
+
+        auto job = std::move(queue.front());
+        queue.pop();
+        return std::move(job);
+    }
+
+    std::unique_ptr<T> Pop() {
+        std::unique_lock<std::mutex> guard(lock);
+        cond.wait(guard, [&]() { return stop || !queue.empty(); });
+        if (stop) {
+            return nullptr;
+        }
+
+        auto job = std::move(queue.front());
+        queue.pop();
+        return std::move(job);
+    }
+
+    void Stop() {
+        std::unique_lock<std::mutex> guard(lock);
+        stop = true;
+        cond.notify_all();
+    }
+
+    void CompleteJob() {
+        std::unique_lock<std::mutex> guard(lock);
+        completedJobs++;
+        if (addedJobs == completedJobs) {
+            stop = true;
+            cond.notify_all();
+        }
+    }
+
+    size_t getUniqSize() {
+        return set.size();
+    }
+
+private:
+    std::queue<std::unique_ptr<T>> queue;
+    std::unordered_set<Uniq> set;
+    std::mutex lock;
+    std::condition_variable cond;
+    bool stop = false;
+
+    size_t addedJobs = 0;
+    size_t completedJobs = 0;
+};
+
+class UrlJob : public IJob<std::string> {
+private:
+    string href;
+
+    string getBodyByHref() {
+        string delimiter = "://";
+        auto finding = href.find(delimiter);
+        if (finding == -1) {
 #ifdef DEBUG
-        cout << href << endl;
+            cout << href << endl;
 
 #endif
+            throw invalid_argument("Protocol is not available");
+        }
+
+        string protocol = href.substr(0, finding);
+        string path = href.substr(finding + delimiter.length(), href.length());
+        if (protocol == "file") {
+            ifstream fin(ROOT_DIR + '/' + path);
+            return string((istreambuf_iterator<char>(fin)),
+                          (istreambuf_iterator<char>()));
+        }
         throw invalid_argument("Protocol is not available");
     }
 
-    string protocol = href.substr(0, finding);
-    string path = href.substr(finding + delimiter.length(), href.length());
-    if (protocol == "file") {
-        ifstream fin(ROOT_DIR + '/' + path);
-        return string((istreambuf_iterator<char>(fin)),
-                      (istreambuf_iterator<char>()));
+public:
+    explicit UrlJob(string &href_) : href(href_) {}
+
+    std::string &getUniq() override {
+        return href;
     }
-    throw invalid_argument("Protocol is not available");
-}
 
-bool checkTreads() {
-    pushLock.lock();
-    for (auto i: threadStatus) {
-        if (i) {
-            pushLock.unlock();
-            return true;
-        }
+    void Execute() override;
+};
 
-    }
-    pushLock.unlock();
-    return false;
-}
+Queue<UrlJob, string> q;
 
-void threadFunc(queue<string> *q, unordered_set<string> *urls, size_t id) {
-    std::thread::id this_id = std::this_thread::get_id();
+void UrlJob::Execute() {
+
+#ifdef DEBUG
+    cout << href << endl;
+#endif
     const regex regexp("<a[^>]* href=\"([^<]+)\"[^>]*>");
     smatch result;
-    while (checkTreads()) {
-//
-        while (!q->empty()) {
-            pushLock.lock();
-            threadStatus[id] = true;
-            if (q->empty())
-            {
-                pushLock.unlock();
-                continue;
-            }
+    if (href.empty())
+        return;
 
-            string h = q->front();
-            q->pop();
+    string body = getBodyByHref();
 
-            pushLock.unlock();
-            string body = getBodyByHref(h);
-#ifdef DEBUG
-            cout << h << endl;
-#endif
-            while (regex_search(body, result, regexp)) {
-                string href = result.str(1);
-                if (href.empty()) {
-                    cout << result.str(0);
-                }
-
-                pushLock.lock();
-                if (!href.empty() && urls->find(href) == urls->end()) {
-                    urls->insert(href);
-                    q->push(href);
-                }
-
-                pushLock.unlock();
-                body = result.suffix().str();
-            }
-
+    while (regex_search(body, result, regexp)) {
+        string newHref = result.str(1);
+        if (newHref.empty()) {
+            body = result.suffix().str();
+            continue;
         }
-        pushLock.lock();
-        threadStatus[id] = false;
-        pushLock.unlock();
+        q.Push(std::make_unique<UrlJob>(newHref));
+        body = result.suffix().str();
     }
-#ifdef DEBUG
-    pushLock.lock();
-    cout << "id" << this_id << " " << "\n";
-    pushLock.unlock();
-#endif
 
 }
 
+void threadFunc() {
+    std::unique_ptr<UrlJob> a = std::move(q.Pop());
+    while (a != nullptr) {
+        a->Execute();
+        q.CompleteJob();
+        a = std::move(q.Pop());
+    }
+}
 
 int main() {
-    unordered_set<string> urls;
-    queue<string> q;
 
     ifstream fin("input.txt");
     ofstream fout("output.html");
@@ -124,14 +180,11 @@ int main() {
     fin >> baseHref >> workersCnt;
 
     vector<thread *> threads;
-    q.push(baseHref);
-    urls.insert(baseHref);
+    q.Push(std::make_unique<UrlJob>(baseHref));
     threads.resize(workersCnt);
-    threadStatus.resize(workersCnt);
 
     for (int i = 0; i < workersCnt; i++) {
-        threads[i] = (new thread(threadFunc, &q, &urls, i));
-        threadStatus[i] = true;
+        threads[i] = (new thread(threadFunc));
     }
     auto start = std::chrono::system_clock::now();
     for (int i = 0; i < workersCnt; i++)
@@ -141,6 +194,7 @@ int main() {
 
     std::chrono::duration<double> elapsed_seconds = end - start;
 
-    cout << "READY " << urls.size() << " pages by " << elapsed_seconds.count() << " s" << endl;
+    cout << "READY " << q.getUniqSize() << " pages by " << elapsed_seconds.count() << " s" << endl;
+
     return 0;
 }
