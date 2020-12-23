7d8ab86c1a636ca6bd067a496aa18d40d95093b8
iprok <i.prokofev@g.nsu.ru>
2020-12-23 19:12:20 +0700

    fixed WebCrawler

--- Ilya_prokofev/Source.cpp
+++ Ilya_prokofev/Source.cpp
@@ -10,24 +10,15 @@
 #include <iostream>
 
 using namespace std;
-set <string> opened_links;
-set <string> links_to_open;
-string start_page;
-mutex opened_links_mutex;
-mutex links_to_open_mutex;
-const string path_to_test_data = "C:\\Users\\iprok\\test_data\\";
-size_t opened_links_cnt = 0;
-size_t links_to_open_cnt = 1;
-const regex re_link("<a href=\"file://[_a-zA-Z0-9]*\\.(\\html)\">");
 
-clock_t last_link_adding_time = clock();
+const string path_to_test_data = "C:\\Users\\iprok\\test_data\\";
+const regex re_link("<a href=\"file://[_a-zA-Z0-9]*\\.(\\html)\">");
 
 string readFile(const string& fileName) {
 	ifstream f(fileName);
-	stringstream ss;
-	ss << f.rdbuf();
+	string content((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
 	f.close();
-	return ss.str();
+	return content;
 }
 
 set <string> open_page(string page) {
@@ -47,48 +38,50 @@ set <string> open_page(string page) {
 	return result;
 }
 
-void index() {
+void index(mutex& links_mutex, mutex& time_mutex, set <string>& opened_links, set <string>& links_to_open, chrono::system_clock::time_point& last_link_adding_time) {
 	string cur_page = "";
-	unique_lock <mutex> unique_opened_links_mutex (opened_links_mutex, defer_lock);
-	unique_lock <mutex> unique_links_to_open_mutex (links_to_open_mutex, defer_lock);
+	
 	while (1) {
-		lock(unique_opened_links_mutex, unique_links_to_open_mutex);
+		links_mutex.lock();
 		if (!links_to_open.empty()) {
 			cur_page = *links_to_open.begin();
 			links_to_open.erase(cur_page);
 			opened_links.insert(cur_page);
-			opened_links_cnt++;
-			unique_opened_links_mutex.unlock();
-			unique_links_to_open_mutex.unlock();
+			links_mutex.unlock();
 		}
 		else {
-			unique_opened_links_mutex.unlock();
-			unique_links_to_open_mutex.unlock();
+			links_mutex.unlock();
 			this_thread::sleep_for(chrono::seconds(10));
 			continue;
 		}
 		set <string> found_pages = open_page(cur_page);
 
-		lock(unique_opened_links_mutex, unique_links_to_open_mutex);
+		links_mutex.lock();
 		for (auto page : found_pages) {
 			if (opened_links.find(page) == opened_links.end() && links_to_open.find(page) == links_to_open.end()) {
 				links_to_open.insert(page);
-				links_to_open_cnt ++;
 			}
 		}
-
-		cout << opened_links_cnt << " " << links_to_open_cnt << endl;
-
-		unique_opened_links_mutex.unlock();
-		unique_links_to_open_mutex.unlock();
+		cout << opened_links.size() << endl;
+		links_mutex.unlock();
+		if (!found_pages.empty()) {
+			time_mutex.lock();
+			last_link_adding_time = chrono::system_clock::now();
+			time_mutex.unlock();
+		}
 	}
 }
 
-void start_indexing(string start_page, size_t threads_num) {
+size_t start_indexing(string start_page, size_t threads_num) {
+	set <string> opened_links;
+	set <string> links_to_open;
+	mutex links_mutex;
+	mutex time_mutex;
 	vector <thread> threads;
+	auto last_link_adding_time = chrono::system_clock::now();
 	links_to_open.insert(start_page);
 	for (size_t i = 0; i < threads_num; i++) {
-		threads.emplace_back(index);
+		threads.emplace_back(index, ref(links_mutex), ref(time_mutex), ref(opened_links), ref(links_to_open), ref(last_link_adding_time));
 	}
 
 	for (auto& t : threads)
@@ -96,9 +89,17 @@ void start_indexing(string start_page, size_t threads_num) {
 		t.detach();
 	}
 
-	while (links_to_open_cnt != opened_links_cnt) {
-		this_thread::sleep_for(chrono::seconds(10));
+
+	while (1) {
+		time_mutex.lock();
+		chrono::duration<double> past_time = chrono::system_clock::now() - last_link_adding_time;
+		time_mutex.unlock();
+		if (past_time.count() > 3) {
+			break;
+		}
+		this_thread::sleep_for(chrono::seconds(2));
 	}
+	return opened_links.size();
 }
 
 
@@ -110,11 +111,11 @@ int main() {
 	size_t workers_num;
 	input >> page_address >> workers_num;
 	auto start = chrono::system_clock::now();
-	start_indexing(page_address, workers_num);
+	auto cnt = start_indexing(page_address, workers_num);
 	auto finish = chrono::system_clock::now();
 	chrono::duration<double> time = finish - start;
 
-	output << opened_links_cnt << " " << time.count();
+	output << cnt << " " << time.count();
 	input.close();
 	output.close();
 	return 0;
