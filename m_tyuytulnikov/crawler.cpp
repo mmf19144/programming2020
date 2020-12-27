5087e14d30938c6404cc0722704dc76788f3519b
Михаил Тютюльников <mishatyty@gmail.com>
2020-12-27 14:57:05 +0700

    New crawler with regex parser

--- m_tyuytulnikov/crawler.cpp
+++ m_tyuytulnikov/crawler.cpp
@@ -6,135 +6,109 @@
 #include <queue>
 #include <chrono>
 #include <map>
+#include <unordered_set>
+#include <regex>
 
-#define PATH "test_data/"
 using namespace std;
 
-queue<string> file_queue;
-map<string, bool> completed_files;
+const string PATH = "test_data/";
 mutex locker;
-map<std::thread::id, bool> threadStatus;
-
-class FilePage {
-private:
-    std::string page_content;
-
-    void getReferences() {
-        for (size_t i = 0; i < page_content.size(); i++) {
-            if (page_content[i] == '<' && i + 10 < page_content.size()) {
-                std::string token = page_content.substr(i + 1, page_content.find('<', i + 1) - 1);
-                if (token.length() > 0 && token[0] == 'a' && token.substr(2, 6) == "href=\"") {
-                    token = token.substr(8, token.find('>', 8) - 9);
-                    if (token.length() > 0) {
-                        locker.lock();
-                        file_queue.push(token);
-                        locker.unlock();
-                    }
-                }
-            }
-        }
-    }
-
-    static string getAddress(const std::string &input) {
-        string delimiter = "://";
-        string NAME = input.substr(input.find(delimiter) + 3, input.size() - delimiter.size());
-        return NAME;
-    }
-
-public:
-    void analyze(const std::string &input) {
-        page_content = "";
-        string full = string(PATH).append(getAddress(input));
-        ifstream fin(full);
-        page_content = string(istreambuf_iterator<char>(fin), istreambuf_iterator<char>());
-        fin.close();
-        getReferences();
-    }
-
-    FilePage() = default;
-
-    ~FilePage() {
-        page_content.clear();
-    }
-};
+map<string, bool> completed_files;
+queue<string> file_queue;
+vector<bool> threadStatus;
 
 bool threadsWorking() {
-    for (auto &i : threadStatus)
-        if (i.second) {
+    std::unique_lock<std::mutex> guard(locker);
+    for (auto i: threadStatus) {
+        if (i) {
             return true;
         }
+    }
     return false;
 }
 
-void run() {
-    auto this_id = std::this_thread::get_id();
-    FilePage page;
+static string getFileName(const string &input) {
+    smatch res;
+    const regex rx("[0-9]*.html");
+    regex_search(input, res, rx);
+    return res.str();
+}
+
+void run(int thread_id) {
+    const regex rx("<a[^>]* href=\"([^<]+)\"[^>]*>");
+
     while (threadsWorking()) {
-        while (true) {
+        while (!file_queue.empty()) {
             locker.lock();
-            if (file_queue.empty()) {
-                locker.unlock();
-                break;
-            }
-            threadStatus[this_id] = true;
-            string str = file_queue.front();
+
+            string file_to_open = file_queue.front();
             file_queue.pop();
             locker.unlock();
 
-            if (!completed_files[str]) {
-                locker.lock();
-                completed_files[str] = true;
-                locker.unlock();
-                page.analyze(str);
+            string page_content;
+            ifstream in;
+            in.open(PATH + file_to_open);
+//            cout << file_to_open << " " << file_queue.size() << " " << completed_files.size() << endl;
+
+            page_content = string(istreambuf_iterator<char>(in), istreambuf_iterator<char>());
+            in.close();
+            smatch regex_res;
+
+            while (regex_search(page_content, regex_res, rx)) {
+                string file_name = getFileName(regex_res[0]);
+                if (!completed_files[file_name]) {
+                    locker.lock();
+                    file_queue.push(file_name);
+                    completed_files[file_name] = true;
+                    locker.unlock();
+                }
+                page_content = regex_res.suffix().str();
             }
         }
-        threadStatus[this_id] = false;
+        threadStatus[thread_id] = false;
     }
 }
 
 class Crawler {
     const size_t num_of_threads;
-    vector<thread *> Threads;
-    vector<bool> isWorking;
-
+    vector<thread> threads;
 public:
-    Crawler(string startPage, size_t num_of_threads) :
+    Crawler(const string &start_file, size_t num_of_threads) :
             num_of_threads(num_of_threads) {
-        file_queue.push(startPage);
-        Threads.resize(num_of_threads);
-        isWorking.resize(num_of_threads);
+        file_queue.push(start_file);
+        completed_files[start_file] = true;
 
-        auto time_start = std::chrono::system_clock::now();
+        threads.resize(num_of_threads);
+        threadStatus.resize(num_of_threads);
+
         for (size_t i = 0; i < num_of_threads; i++) {
-            Threads[i] = new thread(run);
-            auto id = Threads[i]->get_id();
-            threadStatus[id] = true;
+            threads[i] = thread(run, i);
+            threadStatus[i] = true;
         }
     }
 
     pair<unsigned long long int, double> crawl() {
         auto time_start = std::chrono::system_clock::now();
         for (size_t i = 0; i < num_of_threads; i++)
-            Threads[i]->join();
+            threads[i].join();
         auto time_end = std::chrono::system_clock::now();
         std::chrono::duration<double> time = time_end - time_start;
 
-        auto p = make_pair(completed_files.size(), time.count());
-        return p;
-    }
-
-    ~Crawler() {
-        for (size_t i = 0; i < num_of_threads; i++)
-            Threads[i]->~thread();
+        return make_pair(completed_files.size(), time.count());
     }
 };
 
+
 int main() {
     string startPage;
-    size_t num_of_threads;
-    ifstream fin("input.txt");
-    fin >> startPage >> num_of_threads;
-    fin.close();
+    ifstream in;
+    int num_of_threads;
+
+    in.open("input.txt");
+    in >> startPage;
+    startPage = getFileName(startPage);
+    in >> num_of_threads;
+    in.close();
 
     Crawler crawler(startPage, num_of_threads);
     auto res = crawler.crawl();
@@ -142,6 +116,6 @@ int main() {
     ofstream fout("output.txt");
     fout << res.first << " " << res.second;
     fout.close();
-    completed_files.clear();
+
     return 0;
 }
