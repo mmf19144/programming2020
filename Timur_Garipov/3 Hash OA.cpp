5bfd1eff746910cf0782af231ae8e98e2d6f0178
Qishna <53940961+Qishna@users.noreply.github.com>
2020-12-28 20:45:11 +0700

    hash fix

--- Timur_Garipov/3 Hash OA.cpp
+++ Timur_Garipov/3 Hash OA.cpp
@@ -6,268 +6,6 @@
 #include <iostream>
 #include <fstream>
 
-class RowOfMatrix {
-protected:
-    int* arr;
-    int dimension;
-public:
-    int getDimension() const {
-        return this->dimension;
-    }
-    RowOfMatrix() {}
-    RowOfMatrix(int size, int* array) {
-        dimension = size;
-        arr = array;
-    }
-    ~RowOfMatrix() {
-        delete[] arr;
-    }
-    int& operator[](int number) {
-        number--;
-        if (number < 0 || number >= dimension)
-            throw std::string("Wrong number");
-
-        return this->arr[number];
-    }
-};
-
-class ColumnOfMatrix : public RowOfMatrix {
-public:
-    ColumnOfMatrix() {}
-    ColumnOfMatrix(int size, int* array) {
-        dimension = size;
-        arr = array;
-    }
-};
-
-class matrix {
-private:
-    int dimension;
-    int* memory;
-public:
-    int getDimension() const {
-        return this->dimension;
-    }
-    int getElement(int n) const {
-        if (n >= 0 && n < dimension * dimension)
-            return memory[n];
-    }
-    matrix() {
-        this->dimension = 0;
-        this->memory = nullptr;
-    }
-    matrix(int n) {
-        if (n >= 0) {
-            this->dimension = n;
-            this->memory = new int[n * n]{ 0 };
-
-            for (int i = 0; i < n; i++) {
-                this->memory[i + i * n] = 1;
-            }
-        }
-    }
-    matrix(int n, int array[]) {
-        if (n >= 0) {
-            this->dimension = n;
-            this->memory = new int[n * n]{ 0 };
-
-            for (int i = 0; i < n; i++) {
-                this->memory[i + i * n] = array[i];
-            }
-        }
-    }
-    ~matrix() {
-        delete[] memory;
-    }
-    matrix(const matrix& c1) {
-        this->dimension = c1.dimension;
-        int n = this->dimension;
-        int k = n * n;
-
-        this->memory = new int[n * n];
-        for (int i = 0; i < k; i++) {
-            this->memory[i] = c1.memory[i];
-        }
-    }
-    matrix operator()(int a, int b) {
-        int size = this->getDimension();
-        if ((size >= a) && (size >= b)) {
-            matrix minorOfMatrix(size - 1);
-
-            int i_m = 0;
-            int j_m = 0;
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (!((i == a - 1) || (j == b - 1))) {
-                        minorOfMatrix.memory[i_m + minorOfMatrix.dimension * j_m] = this->memory[i + this->dimension * j];
-
-
-                        if (j != b - 1)
-                            j_m++;
-                    }
-                }
-                j_m = 0;
-                if (i != a - 1)
-                    i_m++;
-            }
-            return minorOfMatrix;
-        }
-        else return 0;
-    }
-    RowOfMatrix& operator[](int n) {
-        n--;
-        int size = this->getDimension();
-        if (n < 0 || n >= size) throw "Не существует строки с таким номером";
-
-        int** arrayRow = new int* [dimension];
-        for (int i = 0; i < dimension; i++)
-            arrayRow[i] = &memory[dimension * n + i];
-
-        ColumnOfMatrix* c = new ColumnOfMatrix(dimension, *arrayRow);
-        return *c;
-    }
-    ColumnOfMatrix& operator()(int n) {
-        n--;
-        int size = this->getDimension();
-        if (n < 0 || n >= size) throw "Не существует строки с таким номером";
-
-        int** arrayCol = new int* [dimension];
-        for (int i = 0; i < dimension; i++)
-            arrayCol[i] = &memory[dimension * i + n];
-
-        ColumnOfMatrix* c = new ColumnOfMatrix(dimension, *arrayCol);
-        return *c;
-    }
-    matrix operator=(matrix c) {
-        this->dimension = c.dimension;
-        int n = c.dimension, k = n * n;
-
-        for (int i = 0; i < k; i++) {
-            this->memory[i] = c.memory[i];
-        }
-        return *this;
-    }
-    friend std::istream& operator>>(std::istream& in, matrix& c1);
-    friend std::ostream& operator<<(std::ostream& out, matrix& c1);
-    friend matrix operator+(matrix c1, matrix c2);
-    friend matrix operator-(matrix c1, matrix c2);
-    friend bool operator==(matrix c1, matrix c2);
-    friend matrix operator*(matrix c1, matrix c2);
-    friend matrix operator+(matrix c1);
-};
-
-std::ostream& operator<< (std::ostream& out, matrix& c1) {
-    int size = c1.getDimension();
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            out << c1.memory[i * c1.getDimension() + j] << " ";
-        out << std::endl;
-    }
-
-    return out;
-}
-
-std::istream& operator>>(std::istream& in, matrix& c1) {
-    int size = c1.getDimension();
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            in >> c1.memory[i * c1.getDimension() + j];
-    }
-
-    return in;
-}
-
-matrix operator+(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        int* a = new int[size] { 0 };
-        matrix result(size, a);
-        delete a;
-
-        for (int i = 0; i < size * size; i++) {
-            result.memory[i] = c1.memory[i] + c2.memory[i];
-        }
-        return result;
-    }
-    throw std::string("Сложение матриц разного размера!");
-}
-
-matrix operator-(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        int* a = new int[size] { 0 };
-        matrix result(size, a);
-        delete a;
-
-        for (int i = 0; i < size * size; i++) {
-            result.memory[i] = c1.memory[i] - c2.memory[i];
-        }
-        return result;
-    }
-    throw std::string("Вычитание матриц разного размера!");
-}
-
-matrix operator*(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        matrix result(size, new int[size] { 0 });
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    result.memory[j + size * i] += c1.memory[k + size * i] * c2.memory[j + size * k];
-                }
-            }
-        }
-        return result;
-    }
-    throw std::string("Умножение матриц разного размера!");
-}
-
-bool operator==(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-
-        for (int i = 0; i < size * size; i++)
-            if (c1.memory[i] != c2.memory[i])
-                return false;
-
-        return true;
-    }
-    throw std::string("Сравнение матриц разного размера!");
-}
-
-bool operator!=(matrix c1, matrix c2) {
-    bool flag = (c1 == c2);
-    return !flag;
-}
-
-matrix operator+(matrix c1) {
-    int size = c1.getDimension();
-    if (size == 0)
-        throw std::string("Транспонирование матрицы нулевого размера!");
-
-    int* a = new int[size] { 0 };
-    matrix trans(size, a);
-    delete a;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            trans.memory[i * size + j] = c1.memory[i + j * size];
-    }
-
-    return trans;
-}
-template<> struct std::hash<matrix> {
-    size_t operator()(const matrix& value) {
-        size_t hash = 0;
-        for (int i = 0; i < pow(value.getDimension(), 2); i++) {
-            hash += pow(std::hash<int>()(value.getElement(i)), 2);
-        }
-        return hash;
-    }
-};
-
 template <typename K, typename V>
 class Cell {
 private:
@@ -343,7 +81,6 @@ protected:
         massive = new_massive;
 
 
-
         size_t old_size = size;
         size = new_size;
         for (size_t i = 0; i < old_size; i++)
@@ -398,28 +135,52 @@ protected:
 public:
     class Iterator {
     private:
-        Cell<K, V>* point;
+        size_t index;
+        size_t size, countPair;
+        Cell<K, V>* massive;
     public:
-        Iterator(Cell<K, V>* point1) {
-            point = point1;
+        Iterator(Cell<K, V>* _massive, size_t _size, size_t _countPair, size_t _ind) {
+            massive = _massive;
+            size = _size;
+            index = _ind;
+            countPair = _countPair;
         }
-
         Cell<K, V>& operator++() {
-            for (auto c = this->point; c != end(); *c = *(c + 1)) {
-                if (*c.getStatFill())
-                    return c;
+            if (index < size) {
+                Cell<K, V>* pointer = &massive[index];
+                bool flag = false;
+                for (int i = index + 1; i < size && !flag; i++)
+                    if (massive[i].statFill) {
+                        flag = true;
+                        pointer = &massive[i];
+                    }
+
+                if (flag)
+                    return pointer;
+                else
+                    return &massive[index];
             }
         }
         Cell<K, V>& operator--() {
-            for (auto c = this->point; c != begin(); *c = *(c - 1)) {
-                if (*c.getStatFill())
-                    return c;
+            if (index >= 0) {
+                Cell<K, V>* pointer = &massive[index];
+                bool flag = false;
+                for (int i = index - 1; i >= 0 && !flag; i--)
+                    if (massive[i].statFill) {
+                        flag = true;
+                        pointer = &massive[i];
+                    }
+
+                if (flag)
+                    return pointer;
+                else
+                    return &massive[index];
             }
         }
 
-        bool operator!=(const Iterator It) { return point != It.point; }
-        bool operator==(const Iterator It) { return point == It.point; }
-        Cell<K, V>& operator*() { return *point; }
+        bool operator!=(const Iterator It) { return massive[index] != It.massive[It.index]; }
+        bool operator==(const Iterator It) { return massive[index] == It.massive[It.index]; }
+        Cell<K, V>& operator*() { return &massive[index]; }
     };
     Iterator begin() {
         for (int i = 0; i < size; i++) {
@@ -475,61 +236,6 @@ public:
 };
 
 template <typename K, typename V>
-class MultiHashMap : public HashMap<K, V> {
-private:
-    void iterOfDel(size_t hash_val, const K& delKey) override {
-        if (hash_val >= this->size) {
-            return;
-        }
-
-        if (this->massive[hash_val].getStatFill()) {
-            if (this->massive[hash_val].getKey() == delKey) {
-                this->massive[hash_val].delElement();
-                this->countPairs--;
-            }
-            if (this->massive[hash_val + 1].wasItFilled())
-                iterOfDel(hash_val + 1, delKey);
-        }
-    }
-    std::vector<Cell<K, V>> getAllElementsByKey(const K& findKey) {
-        size_t hash_val = std::hash<K>()(findKey) % this->size;
-        std::vector<Cell<K, V>> support;
-
-        if (hash_val < this->size) {
-            for (; hash_val != this->size && this->massive[hash_val].wasItFilled();) {
-                if (this->massive[hash_val].getStatFill() == true) {
-                    if (this->massive[hash_val].getKey() == findKey)
-                        support.push_back(this->massive[hash_val]);
-                }
-                hash_val++;
-            }
-        }
-        else
-            return nullptr;
-
-        return support;
-    }
-    size_t getCountByKey(const K& findKey) {
-        size_t hash_val = std::hash<K>()(findKey) % this->size;
-        size_t count = 0;
-
-        if (hash_val < this->size) {
-            for (; hash_val != this->size && this->massive[hash_val].wasItFilled();) {
-                if (this->massive[hash_val].getStatFill() == true) {
-                    if (this->massive[hash_val].getKey() == findKey)
-                        count++;
-                }
-                hash_val++;
-            }
-        }
-        else
-            return count;
-
-        return count;
-    }
-};
-
-template <typename K, typename V>
 void startProgram(std::ifstream& fin, const std::string& out) {
     std::ofstream fout(out);
     HashMap<K, V> Table;
@@ -561,8 +267,6 @@ void startProgram(std::ifstream& fin, const std::string& out) {
             continue;
         }
     }
-
-    auto s = HashMap<K, V>::Iterator(Table.begin());
 
     size_t* arrInfo;
     arrInfo = Table.getMainInfo();
