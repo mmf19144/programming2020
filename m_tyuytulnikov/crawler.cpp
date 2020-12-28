59feca2a1bba2c350fe5dfdaff5eed7f0a2d2829
Михаил Тютюльников <mishatyty@gmail.com>
2020-12-28 15:01:11 +0700

    crawler fixes

--- m_tyuytulnikov/crawler.cpp
+++ m_tyuytulnikov/crawler.cpp
@@ -6,7 +6,6 @@
 #include <queue>
 #include <chrono>
 #include <map>
-#include <unordered_set>
 #include <regex>
 
 using namespace std;
@@ -16,15 +15,6 @@ mutex locker;
 
 vector<bool> threadStatus;
 
-bool threadsWorking() {
-    std::unique_lock<std::mutex> guard(locker);
-    for (auto i: threadStatus) {
-        if (i) {
-            return true;
-        }
-    }
-    return false;
-}
 
 static string getFileName(const string &input) {
     smatch res;
@@ -33,60 +23,30 @@ static string getFileName(const string &input) {
     return res.str();
 }
 
-void run(int thread_id, map<string, bool> &completed_files, queue<string> &file_queue) {
-    const regex rx("<a[^>]* href=\"([^<]+)\"[^>]*>");
+const regex rx("<a[^>]* href=\"([^<]+)\"[^>]*>");
 
-    while (threadsWorking()) {
-        while (!file_queue.empty()) {
-            locker.lock();
 
-            string file_to_open = file_queue.front();
-            file_queue.pop();
-            locker.unlock();
-
-            string page_content;
-            ifstream in;
-            in.open(PATH + file_to_open);
-//            cout << file_to_open << " " << file_queue.size() << " " << completed_files.size() << endl;
-
-            page_content = string(istreambuf_iterator<char>(in), istreambuf_iterator<char>());
-            in.close();
-            smatch regex_res;
-
-            while (regex_search(page_content, regex_res, rx)) {
-                string file_name = getFileName(regex_res[0]);
-                if (!completed_files[file_name]) {
-                    locker.lock();
-                    file_queue.push(file_name);
-                    completed_files[file_name] = true;
-                    locker.unlock();
-                }
-                page_content = regex_res.suffix().str();
-            }
-        }
-        threadStatus[thread_id] = false;
-    }
-}
+/*
+ *
+ *    ^
+ *   | |  - Elon Mask is genius
+ *  <| |>
+ *    *
+ *    *
+ *    *
+ */
 
 class Crawler {
     const size_t num_of_threads;
     vector<thread> threads;
     map<string, bool> completed_files;
     queue<string> file_queue;
+    mutex m;
+    bool stop = false;
+    condition_variable conditionVariable;
+    size_t available_tasks = 0;
 public:
-    Crawler(const string &start_file, size_t num_of_threads) :
-            num_of_threads(num_of_threads) {
-        file_queue.push(start_file);
-        completed_files[start_file] = true;
-
-        threads.resize(num_of_threads);
-        threadStatus.resize(num_of_threads);
-
-        for (size_t i = 0; i < num_of_threads; i++) {
-            threads[i] = thread(run, i, ref(completed_files), ref(file_queue));
-            threadStatus[i] = true;
-        }
-    }
+    Crawler(const string &start_file, size_t num_of_threads);
 
     pair<unsigned long long int, double> crawl() {
         auto time_start = std::chrono::system_clock::now();
@@ -97,7 +57,79 @@ public:
 
         return make_pair(completed_files.size(), time.count());
     }
+
+    string queuePop() {
+        std::unique_lock<std::mutex> guard(m);
+        conditionVariable.wait(guard, [&]() { return stop || !file_queue.empty(); });
+        if (stop) {
+            return "";
+        }
+        auto tmp = file_queue.front();
+        file_queue.pop();
+        return tmp;
+    }
+
+    void queuePush(string &s) {
+        std::unique_lock<std::mutex> guard(m);
+        if (s.empty() || completed_files[s]) return;
+        available_tasks++;
+        file_queue.push(s);
+        conditionVariable.notify_one();
+        completed_files[s] = true;
+    }
+
+    void taskReady(){
+        std::unique_lock<std::mutex> guard(m);
+        available_tasks--;
+        if (!available_tasks){
+            stop = true;
+            conditionVariable.notify_all();
+        }
+    }
+
 };
+
+
+void run(int thread_id, Crawler *cr) {
+
+
+    while (true) {
+        string file_to_open = cr->queuePop();
+        if (file_to_open.empty())
+            break;
+        string page_content;
+        ifstream in;
+        in.open(PATH + file_to_open);
+//        cout << file_to_open << endl;
+
+        page_content = string(istreambuf_iterator<char>(in), istreambuf_iterator<char>());
+        in.close();
+        smatch regex_res;
+
+        while (regex_search(page_content, regex_res, rx)) {
+            string file_name = getFileName(regex_res[0]);
+            cr->queuePush(file_name);
+            page_content = regex_res.suffix().str();
+        }
+        cr->taskReady();
+    }
+    threadStatus[thread_id] = false;
+
+}
+
+Crawler::Crawler(const string &start_file, size_t num_of_threads) : num_of_threads(num_of_threads) {
+    file_queue.push(start_file);
+    completed_files[start_file] = true;
+    available_tasks++;
+    threads.resize(num_of_threads);
+    threadStatus.resize(num_of_threads);
+
+    for (size_t i = 0; i < num_of_threads; i++) {
+        threads[i] = thread(run, i, this);
+        threadStatus[i] = true;
+    }
+
+}
 
 
 int main() {
