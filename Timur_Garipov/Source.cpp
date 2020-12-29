a3c49515745180f1e0d71ad3eba00a63c1f988b8
Qishna <53940961+Qishna@users.noreply.github.com>
2020-12-29 07:49:50 +0700

    Crawler fix

--- Timur_Garipov/Source.cpp
+++ Timur_Garipov/Source.cpp
@@ -1,379 +1,146 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <queue>
+#include <chrono>
+#include <regex>
+#include <map>
+#include <set>
 
-class row_m {
+const std::string STORE = "store/";
+const std::string PATH = "test_data/";
+const std::regex regExpr("<a href=\"([^<]+)\">");
+using namespace std;
+
+class Web_page {
 private:
-    int dimension;
+    mutex* locker;
+    queue<string>* queueRef;
+    std::string content;
+    std::set<std::string> ref;
+    int getPosition(const std::string& str, const int& i1, char symb) {
+        for (int i = i1; i < str.length(); i++) {
+            if (str[i] == symb)
+                return i;
+        }
+        return i1;
+    }
+    void getReference() {
+        std::smatch result;
+        while (regex_search(content, result, regExpr)) {
+            std::unique_lock<std::mutex> guard(*locker);
+
+            queueRef->push(result[1]);
+            ref.insert(result[1]);
+            content = result.suffix().str();
+        }
+    }
+    string getProtocol(const std::string& request) {
+        string delimetr = "://";
+        string NAME = request.substr(0, request.find(delimetr));
+        return NAME;
+    }
+    string getAddress(const std::string& request) {
+        string delimetr = "://";
+        string NAME = request.substr(request.find(delimetr) + 3, request.size() - delimetr.size());
+        return NAME;
+    }
+    string getNameOfFIle(const std::string& address) {
+        int i;
+        for (i = address.size() - 1; ; --i)
+            if (address[i] == '/')
+                break;
+        i++;
+        return address.substr(i, address.size() - 1);
+    }
 public:
-    int** arr;
+    void copy(const std::string& address) {
+        std::string out(STORE);
+        out.append(getNameOfFIle(address));
 
-    int getDimension() {
-        return this->dimension;
+        ofstream fout(out);
+        fout << content;
+        fout.close();
     }
-
-    row_m() {}
-    ~row_m() {
-        delete[] arr;
+    static Web_page buildPage(const std::string& input, mutex& lock, queue<string>& q_Ref) {
+        Web_page web;
+        web.queueRef = &q_Ref;
+        web.locker = &lock;
+        web.content = "";
+        web.ref.clear();
+        string full = string(PATH).append(web.getAddress(input));
+        ifstream fin(full);
+        web.content = string(istreambuf_iterator<char>(fin), istreambuf_iterator<char>());
+        fin.close();
+        web.getReference();
+        return web;
     }
-    row_m(row_m& c) {
-        this->dimension = c.dimension;
-        int n = c.dimension;
-        this->arr = new int* [n];
-
-        for (int i = 0; i < n; i++)
-            this->arr[i] = c.arr[i];
-    }
-    row_m(const row_m& c) {
-        this->dimension = c.dimension;
-        int n = c.dimension;
-        this->arr = new int* [n];
-
-        for (int i = 0; i < n; i++)
-            this->arr[i] = c.arr[i];
-    }
-    void buildCol(int dim, int* memory, int number) {
-        if (dim > 0) {
-            this->dimension = dim;
-            this->arr = new int* [dim];
-
-            for (int i = 0; i < dim; i++) {
-                this->arr[i] = &memory[number + dim * i];
-            }
-        }
-    }
-    void buildRow(int dim, int* memory, int number) {
-        if (dim > 0) {
-            this->dimension = dim;
-            this->arr = new int* [dim];
-
-            for (int i = 0; i < dim; i++) {
-                this->arr[i] = &memory[number * dim + i];
-            }
-        }
-    }
-
-    int operator[](int number) {
-        number--;
-        if (number < 0 || number >= dimension)
-            throw std::string("Нет элемента с таким номером");
-
-        return *this->arr[number];
-    }
-
-    friend std::istream& operator>>(std::istream& in, row_m& c1);
-    friend std::ostream& operator<<(std::ostream& out, row_m& c1);
+    Web_page() { }
+    ~Web_page() { }
 };
 
-std::ostream& operator<<(std::ostream& out, row_m& row1) {
-    int size = row1.getDimension();
-    for (int i = 0; i < size; i++) {
-        out << row1.arr[i] << "\t";
-    }
+void startProgram(map<std::thread::id, bool>& threadMap, set<string>& wasHere,
+                  queue<string>& q_Ref, mutex& locker) {
+    auto this_id = std::this_thread::get_id();
+    Web_page web;
 
-    out << std::endl;
-    return out;
+    while (true) {
+        locker.lock();
+        if (q_Ref.empty()) {
+            locker.unlock();
+            break;
+        }
+
+        threadMap[this_id] = true;
+        string str = q_Ref.front();
+        q_Ref.pop();
+        locker.unlock();
+
+        std::unique_lock<std::mutex> guard(locker);
+        if (wasHere.count(str) == 0) {
+            wasHere.insert(str);
+            guard.unlock();
+
+            web = Web_page::buildPage(str, locker, q_Ref);
+        }
+    }
+    threadMap[this_id] = false;
 }
 
-std::istream& operator>>(std::istream& in, row_m& row1) {
-    int size = row1.getDimension();
-    for (int i = 0; i < size; i++) {
-        in >> *row1.arr[i];
+int main()
+{
+    int countThread;
+    queue<string> q_Ref;
+    set<string> wasHere;
+    map<std::thread::id, bool> threadMap;
+    vector<thread> Threads;
+    vector<bool> isWorking;
+    mutex locker;
+
+    string start;
+    ifstream fin("input.txt");
+    fin >> start >> countThread;
+    fin.close();
+    q_Ref.push(start);
+    isWorking.resize(countThread);
+
+    auto time_start = std::chrono::system_clock::now();
+    for (int i = 0; i < countThread; i++) {
+        Threads.emplace_back(startProgram, ref(threadMap), ref(wasHere), ref(q_Ref), ref(locker));
+        auto id = Threads[i].get_id();
+        threadMap[id] = true;
     }
+    for (int i = 0; i < countThread; i++)
+        Threads[i].join();
+    auto time_end = std::chrono::system_clock::now();
 
-    return in;
-}
+    std::chrono::duration<double> time = time_end - time_start;
+    ofstream fout("output.txt");
+    fout << wasHere.size() << " " << time.count() << std::endl;
+    fout.close();
+    wasHere.clear();
 
-class matrix {
-private:
-    int dimension;
-    int* memory;
-public:
-    row_m* row,
-        * column;
-
-    int getDimension() {
-        return this->dimension;
-    }
-    matrix() {
-        this->dimension = 0;
-        this->column = nullptr;
-        this->row = nullptr;
-        this->memory = nullptr;
-    }
-    matrix(int n) {
-        if (n >= 0) {
-            this->dimension = n;
-            this->memory = new int[n * n]{ 0 };
-
-            this->column = new row_m[n];
-            this->row = new row_m[n];
-
-            for (int i = 0; i < n; i++) {
-                this->memory[i + i * n] = 1;
-            }
-
-            for (int i = 0; i < n; i++) {
-                this->column[i].buildCol(n, this->memory, i);
-                this->row[i].buildRow(n, this->memory, i);
-            }
-        }
-    }
-    matrix(int n, int array[]) {
-        if (n >= 0) {
-            this->dimension = n;
-            this->memory = new int[n * n]{ 0 };
-
-            this->column = new row_m[n];
-            this->row = new row_m[n];
-
-            for (int i = 0; i < n; i++) {
-                this->memory[i + i * n] = array[i];
-            }
-
-            for (int i = 0; i < n; i++) {
-                this->column[i].buildCol(n, this->memory, i);
-                this->row[i].buildRow(n, this->memory, i);
-            }
-        }
-    }
-    ~matrix() {
-        delete[] column;
-        delete[] row;
-        delete[] memory;
-    }
-    matrix(matrix& c1) {
-        this->dimension = c1.dimension;
-        int n = this->dimension;
-        int k = n * n;
-
-        //this->memory = c1.memory;
-        //this->column = c1.column;
-        //this->row = c1.row;
-
-        this->memory = new int[n * n];
-        for (int i = 0; i < k; i++) {
-            this->memory[i] = c1.memory[i];
-        }
-
-        this->column = new row_m[n];
-        this->row = new row_m[n];
-
-        for (int i = 0; i < n; i++) {
-            this->column[i].buildCol(n, this->memory, i);
-            this->row[i].buildRow(n, this->memory, i);
-        }
-    }
-    matrix(const matrix& c1) {
-        this->dimension = c1.dimension;
-        int n = this->dimension;
-        int k = n * n;
-
-        //this->memory = c1.memory;
-        //this->column = c1.column;
-        //this->row = c1.row;
-
-        this->memory = new int[n * n];
-        for (int i = 0; i < k; i++) {
-            this->memory[i] = c1.memory[i];
-        }
-
-        this->column = new row_m[n];
-        this->row = new row_m[n];
-
-        for (int i = 0; i < n; i++) {
-            this->column[i].buildCol(n, this->memory, i);
-            this->row[i].buildRow(n, this->memory, i);
-        }
-    }
-    matrix operator()(int a, int b) {
-        int size = this->getDimension();
-        if ((size >= a) && (size >= b)) {
-            matrix minor_m(size - 1);
-
-            int i_m = 0;
-            int j_m = 0;
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    if (!((i == a - 1) || (j == b - 1))) {
-                        *minor_m.row[i_m].arr[j_m] = *this->row[i].arr[j];
-                        if (j != b - 1)
-                            j_m++;
-                    }
-                }
-                j_m = 0;
-                if (i != a - 1)
-                    i_m++;
-            }
-            return minor_m;
-        }
-        else return NULL;
-    }
-    friend std::istream& operator>>(std::istream& in, matrix& c1);
-    friend std::ostream& operator<<(std::ostream& out, matrix& c1);
-    row_m operator[](int n) {
-        n--;
-        int size = this->getDimension();
-        if (n < 0 || n >= size) throw "Не существует строки с таким номером";
-
-        return this->row[n];
-    }
-
-    row_m operator()(int n) {
-        n--;
-        int size = this->getDimension();
-        if (n < 0 || n >= size)
-            throw "Не существует столбца с таким номером";
-
-        return this->column[n];
-    }
-    matrix operator=(matrix c) {
-        this->dimension = c.dimension;
-        int n = c.dimension, k = n * n;
-
-        for (int i = 0; i < k; i++) {
-            this->memory[i] = c.memory[i];
-        }
-        return *this;
-    }
-};
-
-std::ostream& operator<< (std::ostream& out, matrix& c1) {
-    int size = c1.getDimension();
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            out << *c1.row[i].arr[j] << " ";
-        out << std::endl;
-    }
-
-    return out;
-}
-
-std::istream& operator>>(std::istream& in, matrix& c1) {
-    int size = c1.getDimension();
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            in >> *c1.row[i].arr[j];
-    }
-
-    return in;
-}
-
-matrix operator+(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        int* a = new int[size] { 0 };
-        matrix result(size, a);
-        delete a;
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                *result.column[i].arr[j] = *c1.column[i].arr[j] + *c2.column[i].arr[j];
-            }
-        }
-        return result;
-    }
-    throw std::string("Сложение матриц разного размера!");
-}
-
-matrix operator-(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        int* a = new int[size] { 0 };
-        matrix result(size, a);
-        delete a;
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                *result.column[i].arr[j] = *c1.column[i].arr[j] - *c2.column[i].arr[j];
-            }
-        }
-        return result;
-    }
-    throw std::string("Вычитание матриц разного размера!");
-}
-
-matrix operator*(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-        int* a = new int[size] { 0 };
-        matrix result(size, a);
-        delete a;
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    *result.row[i].arr[j] += *c1.row[i].arr[k] * *c2.column[j].arr[k];
-                }
-            }
-        }
-        return result;
-    }
-    throw std::string("Умножение матриц разного размера!");
-}
-
-bool operator==(matrix c1, matrix c2) {
-    if (c1.getDimension() == c2.getDimension()) {
-        int size = c1.getDimension();
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (*c1.column[i].arr[j] != *c2.column[i].arr[j])
-                    return false;
-            }
-        }
-
-        return true;
-    }
-    throw std::string("Сравнение матриц разного размера!");
-}
-
-bool operator!=(matrix c1, matrix c2) {
-    bool flag = (c1 == c2);
-    return !flag;
-}
-
-matrix operator+(matrix c1) {
-    int size = c1.getDimension();
-    if (size == 0)
-        throw std::string("Транспонирование матрицы нулевого размера!");
-
-    int* a = new int[size] { 0 };
-    matrix trans(size, a);
-    delete a;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            *trans.row[i].arr[j] = *c1.column[i].arr[j];
-    }
-
-    return trans;
-}
-
-int main() {
-    std::ifstream fin("input.txt");
-    std::ofstream fout("output.txt");
-
-    int N;
-    int k;
-
-    fin >> N >> k;
-
-    int* diag = new int[N] {k};
-    for (int i = 0; i < N; i++)
-        diag[i] = k;
-
-    matrix A(N);
-    matrix B(N);
-    matrix C(N);
-    matrix D(N);
-    matrix K(N, diag);
-    delete diag;
-
-    fin >> A >> B >> C >> D;
-
-    matrix example_result = ((A + B * (+C) + K) * (+D));
-
-    fout << example_result;
-    return 0;
-
+    return EXIT_SUCCESS;
 }
